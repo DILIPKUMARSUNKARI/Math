57d5cbb98c3fe633b0153746b2977c618c6ba529
DILIP KUMAR SUNKARI <104927524+DILIPKUMARSUNKARI@users.noreply.github.com>
2023-05-24 13:33:15 +0530

    Update cal.groovy

--- vars/cal.groovy
+++ vars/cal.groovy
@@ -1,12 +1,12 @@
 def add(a,b)
 {
-  echo "the sum of ${a} and ${b} is {a+b}"
+  echo "the sum of ${a} and ${b} is ${a+b}"
 }
 def sub(a,b)
 {
-  echo "the Sub of ${a} and ${b} is {a-b}"
+  echo "the Sub of ${a} and ${b} is ${a-b}"
 }
 def mul(a,b)
 {
-  echo "the Sub of ${a} and ${b} is {a*b}"
+  echo "the Sub of ${a} and ${b} is ${a*b}"
 }
